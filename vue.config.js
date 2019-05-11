e155d1b01b572bd6d2783c15b006860d8730d8de
Eddie Dunn <eddie.dunn+git@gmail.com>
2019-05-11 22:32:38 +0200

    Add vue-plugin-svg-sprit

--- vue.config.js
+++ vue.config.js
@@ -16,6 +16,7 @@ module.exports = {
       },
     },
   },
+
   pluginOptions: {
     i18n: {
       locale: "en",
@@ -24,11 +25,19 @@ module.exports = {
       enableInSFC: true,
     },
   },
+
   devServer: {
     // open: process.platform === 'darwin',
     host: "0.0.0.0",
     port: 3000, // CHANGE YOUR PORT HERE!
     // https: false,
     // hotOnly: false,
+  },
+
+  chainWebpack: (config) => {
+    config.module
+      .rule("svg-sprite")
+      .use("svgo-loader")
+      .loader("svgo-loader")
   },
 }
