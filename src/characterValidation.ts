c41f0816dd36560ea1b8568759933a9420ca3a97
Edward Dunn Ekelund <eddie.dunn@netset.com>
2019-05-14 16:05:38 +0200

    Add stub characterValidation module

---
+++ src/characterValidation.ts
@@ -0,0 +1,18 @@
+// TODO: Move character validation here
+import { CharacterData } from "@/characterData"
+
+export function validateBaseNew(characterData: CharacterData): boolean {
+  const valid =
+    !!characterData.name &&
+    // !!characterData.sex &&
+    !!characterData.age &&
+    !!characterData.kin &&
+    !!characterData.class
+  return valid
+}
+
+function validateAttributesNew() {}
+
+function validateTalentsNew() {}
+
+function validateSkillsNew() {}
