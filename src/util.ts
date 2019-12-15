61de1f47700d09db51d84b3b11f056eca977a437
Eddie Dunn <eddie.dunn@gmail.com>
2019-12-15 03:22:31 +0100

    src/util.ts: Add Result, logging, randomName

--- src/util.ts
+++ src/util.ts
@@ -7,3 +7,66 @@ export function timeout(delay: number) {
     setTimeout(resolve, delay)
   })
 }
+
+export interface Result {
+  success: boolean
+  data?: any
+  error?: string
+}
+
+export const OK = (data?: any): Result => ({ success: true, data })
+export const Err = (error?: string, data?: any): Result => ({
+  success: false,
+  data,
+  error,
+})
+
+export const Ack = OK
+export const Nack = Err
+
+/* eslint-disable no-console */
+export const errlog = console.error
+export const log = console.log
+/* eslint-enable no-console */
+
+const NAMES = [
+  "Alice",
+  "Bob",
+  "Charles",
+  // radio
+  "Alpha",
+  "Bravo",
+  "Charlie",
+  "Delta",
+  "Echo",
+  "Foxtrot",
+  "Golf",
+  "Hotel",
+  "India",
+  "Juliet",
+  "Kilo",
+  "Lima",
+  "Mike",
+  "November",
+  "Oscar",
+  "Papa",
+  "Quebec",
+  "Romeo",
+  "Sierra",
+  "Tango",
+  "Uniform",
+  "Victor",
+  "Whiskey",
+  "X-ray",
+  "Yankee",
+  "Zulu",
+]
+
+export function randomName(words = 1, nameList: string[] = NAMES): string {
+  const getName = () => nameList[Math.floor(Math.random() * nameList.length)]
+  let names = []
+  for (let i = 0; i < words; i++) {
+    names.push(getName())
+  }
+  return names.join(" ")
+}
