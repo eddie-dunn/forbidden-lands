35236038f48c9b8f279d632595cd1436b13f440c
Eddie Dunn <eddie.dunn+git@gmail.com>
2020-01-03 06:38:33 +0100

    eventBus: Add function to send char update event

--- src/util/eventBus.ts
+++ src/util/eventBus.ts
@@ -1,7 +1,19 @@
 import Vue from "vue"
+import { Protocol, ProtocolTypes } from "@/components/multiplay/protocol"
+import { CharData } from "@/characterData"
+
 export const EventBus = new Vue()
 
 export enum BusEvent {
   notification = "event notification",
   characterUpdate = "char update",
+}
+
+export function eventSendCharData(charData: CharData, peerId = "") {
+  const busData: Protocol.MsgCharacter = {
+    type: ProtocolTypes.charData,
+    peerId,
+    character: charData,
+  }
+  EventBus.$emit(BusEvent.characterUpdate, busData)
 }
