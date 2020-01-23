3a5009bb353fe319f288ac2e9b64f0bc16aea0b9
Edward Dunn Ekelund <eddie.dunn@netset.com>
2020-01-23 15:47:21 +0100

    App: Remove global button CSS

--- src/App.vue
+++ src/App.vue
@@ -321,68 +321,6 @@ select {
   }
 }
 
-// &::-moz-focus-outer {
-//   border: 0;
-// }
-
-// TODO: Replace all instances of class="button" with FLButton
-.button {
-  &::-moz-focus-inner {
-    border: 0;
-  }
-  outline: 0;
-  text-decoration: none;
-  padding: 0.5rem 2rem;
-  display: inline-block;
-  color: #fff;
-  background-color: @pastel-green;
-  transition: all 0.15s ease;
-  box-sizing: border-box;
-  border: 1px solid #4fc08d;
-  cursor: pointer;
-  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
-    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
-
-  &:active {
-    transform: scale(0.97);
-  }
-  &:focus {
-    outline: @color-danger dotted 1px;
-  }
-
-  &-red,
-  &-danger {
-    color: #fff;
-    background-color: @pastel-red;
-    border: 1px solid @pastel-red;
-  }
-
-  &-white,
-  &-cancel {
-    background-color: white;
-    color: #4fc08d;
-  }
-
-  &-shadow,
-  &-ghost {
-    border: 0;
-    box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
-      0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
-  }
-
-  &:disabled {
-    background: slategray;
-    border: 1px solid slategray;
-    color: lightgray;
-    &:hover {
-      cursor: not-allowed;
-    }
-    &:active {
-      transform: unset;
-    }
-  }
-}
-
 .flex-row-wrap {
   display: flex;
   flex-direction: row;
