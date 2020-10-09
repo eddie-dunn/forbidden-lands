f2de1069f2e346805cef9f42312635e0c722498f
Eddie Dunn <eddie.dunn+git@gmail.com>
2020-10-09 14:53:51 +0200

    FLInput: Add error props

--- src/components/base/FLInput.vue
+++ src/components/base/FLInput.vue
@@ -18,6 +18,11 @@ export default class FLInput extends Vue {
   @Prop({ default: "" }) iconName!: string
   @Prop({ default: false }) required!: boolean
   @Prop({ default: () => {} }) enterCb!: Function
+  @Prop({ default: false }) readonly error!: boolean
+  @Prop({ default: "" }) readonly errorText!: string
+  @Prop({ default: () => {} }) readonly validate!: Function
+
+  showError = this.error
 
   get _id(): string {
     return this.id || uuid1()
@@ -25,6 +30,17 @@ export default class FLInput extends Vue {
 
   inputEvent($event: any) {
     this.$emit("input", $event.target.value)
+  }
+
+  onBlur() {
+    if (!this.validate || this.error) {
+      return this.error
+    }
+    if (!this.validate()) {
+      this.showError = true
+    } else {
+      this.showError = false
+    }
   }
 }
 </script>
@@ -40,12 +56,16 @@ export default class FLInput extends Vue {
       v-bind="$attrs"
       :required="required"
       :id="_id"
-      class="input"
+      :class="['input', showError && 'error']"
       :type="type"
       v-on:keyup.enter.exact="() => enterCb && enterCb()"
       @input="inputEvent"
       :value="value"
+      @blur="() => onBlur()"
     />
+    <div v-if="showError && errorText" class="error-text font-small">
+      {{ errorText }}
+    </div>
   </div>
 </template>
 
@@ -58,9 +78,38 @@ export default class FLInput extends Vue {
   align-items: flex-start;
 }
 
+.error-text {
+  color: @pastel-red;
+}
+
+label {
+  white-space: nowrap;
+}
+
+// input[type="number"],
+// input[type="text"] {
+//   min-height: 1rem;
+//   background-color: @background-color;
+//   color: @slate-black;
+//   height: auto;
+//   min-height: 1rem;
+//   padding: 0.3rem;
+//   appearance: none;
+//   inline-size: auto;
+//   box-sizing: border-box;
+//   &:disabled {
+//     background-image: none;
+//     border: 2px solid ~"@{pastel-green}55";
+//   }
+// }
+
 .input {
   width: 100%;
+  border: 2px solid @pastel-green-transparent;
   box-sizing: border-box;
+  &.error {
+    border: 2px solid @pastel-red;
+  }
 }
 
 .required-sign {
